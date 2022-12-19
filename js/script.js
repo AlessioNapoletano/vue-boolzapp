bbf9baa17c1856b76f350b230858c7ab3ab9cc6c
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-12-19 13:08:28 +0100

    add hover in message, for delete message

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            hover: false,
+            
             newTextSearch: '',
             newTextMessage: '',
             currentChat: 0,
@@ -18,16 +18,19 @@ createApp({
                             date: '10/01/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent',
+                            
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di stendere i panni',
-                            status: 'sent'
+                            status: 'sent',
+                            
                         },
                         {
                             date: '10/01/2020 16:15:22',
                             message: 'Tutto fatto!',
-                            status: 'received'
+                            status: 'received',
+                            
                         }
                     ],
                 },
@@ -202,9 +205,15 @@ createApp({
         },
 
         deleteMessage: function(index) {
+            console.log(index);
             alert("Il messaggio: " + " \' "+ this.contacts[this.currentChat].messages[index].message + " \' " + " sarà Eliminato definitivamente dalla chat")
             this.contacts[this.currentChat].messages.splice(index, 1);
 
+        },
+
+        showIndex: function(index) {
+            console.log(index);
+            console.log(this.contacts[this.currentChat].messages[index])
         }
 
     }
